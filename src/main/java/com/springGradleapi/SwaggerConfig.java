4f2e5f2cbd0a5a86edba60578f6271d2634aeaf5
Mahnaz-karimi <72239384+Mahnaz-karimi@users.noreply.github.com>
2023-07-28 03:06:20 +0200

    modified kafka configuration and swagger

--- src/main/java/com/springGradleapi/SwaggerConfig.java
+++ src/main/java/com/springGradleapi/SwaggerConfig.java
@@ -14,7 +14,7 @@ public class SwaggerConfig {
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
-                .apis(RequestHandlerSelectors.basePackage("com.springGradleapi"))
+                .apis(RequestHandlerSelectors.basePackage("com.springGradleapi.controller"))
                 .build();
     }
 }
